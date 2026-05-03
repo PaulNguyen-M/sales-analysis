a49b1108dc4d51f0596af1d94cfc9e16d4c3c2df
anhPaul2005 <tanh29012004@gmail.com>
2026-05-04 00:30:04 +0700

    fix code in folder data_analysis

--- data_analysis/src/feature_engineering.py
+++ data_analysis/src/feature_engineering.py
@@ -1,6 +1,10 @@
 import numpy as np
+import pandas as pd
 
 def add_feature(df):
+
+    df['Ngay_Xuat'] = pd.to_datetime(df['Ngay_Xuat'], errors='coerce')
+
     df['Profit'] = df['Doanh_Thu'] - df['Tien_Nhap']
     df['Month'] = df['Ngay_Xuat'].dt.month
     df['Promo'] = df['SL_Xuat_KM'] > 0
